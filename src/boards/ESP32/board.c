0c77dd7ca8dc99d82a58dc1aad2af6e5af40119f
Michael Keyser <ma.keyser28@gmail.com>
2022-08-03 15:32:33 -0600

    put in dummy functions, project now builds

--- src/boards/ESP32/board.c
+++ src/boards/ESP32/board.c
@@ -15,6 +15,7 @@ Sets up the ESP and its peripherals
 #include "freertos/task.h"
 
 
+#include "esp_system.h"
 
 #include <stdio.h>
 #include "esp_log.h"
@@ -25,15 +26,126 @@ void BoardInitMcu()
     
 }
 
-void BoardLowPowerHandler( void )
+/*!
+ * \brief Resets the mcu.
+ */
+void BoardResetMcu( void )
 {
-
+    abort();
 }
 
 void BoardInitPeriph( void )
 {
     // Initalize RTC timers
     RtcInit();
+}
+
+/*!
+ * \brief De-initializes the target board peripherals to decrease power
+ *        consumption.
+ */
+void BoardDeInitMcu( void )
+{
+    abort();
+}
+
+
+
+/*!
+ * \brief Gets the current potentiometer level value
+ *
+ * \retval value  Potentiometer level ( value in percent )
+ */
+uint8_t BoardGetPotiLevel( void )
+{
+    abort();
+    return 0;
+}
+
+/*!
+ * \brief Measure the Battery voltage
+ *
+ * \retval value  battery voltage in volts
+ */
+uint32_t BoardGetBatteryVoltage( void )
+{
+    abort();
+    return 0;
+}
+
+/*!
+ * \brief Get the current battery level
+ *
+ * \retval value  battery level [  0: USB,
+ *                                 1: Min level,
+ *                                 x: level
+ *                               254: fully charged,
+ *                               255: Error]
+ */
+uint8_t BoardGetBatteryLevel( void )
+{
+    abort();
+    return 0;
+}
+
+/*!
+ * \brief Get the current MCU temperature in degree celcius * 256
+ *
+ * \retval temperature * 256
+ */
+int16_t BoardGetTemperature( void )
+{
+    abort();
+    return 0;
+}
+
+/*!
+ * Returns a pseudo random seed generated using the MCU Unique ID
+ *
+ * \retval seed Generated pseudo random seed
+ */
+uint32_t BoardGetRandomSeed( void )
+{
+    abort();
+    return 0;
+}
+
+/*!
+ * \brief Gets the board 64 bits unique ID
+ *
+ * \param [IN] id Pointer to an array that will contain the Unique ID
+ */
+void BoardGetUniqueId( uint8_t *id )
+{
+    abort();
+}
+
+void BoardLowPowerHandler( void )
+{
+    abort();
+}
+
+/*!
+ * \brief Get the board power source
+ *
+ * \retval value  power source [0: USB_POWER, 1: BATTERY_POWER]
+ */
+uint8_t GetBoardPowerSource( void )
+{
+    abort();
+    return 0;
+}
+
+/*!
+ * \brief Get the board version
+ *
+ * \retval value  Version
+ */
+Version_t BoardGetVersion( void )
+{
+    Version_t v;
+    abort();
+    return v;
 }
 
 
@@ -55,3 +167,6 @@ void BoardCriticalSectionEnd( uint32_t *mask )
    //xt_ints_on((unsigned int)*mask );
    taskEXIT_CRITICAL_ISR(&lock_name_original);
 }
+
+
+
